d9c6821fca9e49418c96e553256a60ded3d75b5f
Pedro Concejero <pedro.concejerocerezo@gmail.com>
2026-03-31 16:37:57 +0200

    Add files via upload

---
+++ 08_shinydashboard/08_shinydashboard_app_00.R
@@ -0,0 +1,30 @@
+# shinydashboard
+# Para Curso visualización avanzada master UNED 2026
+
+# A partir de
+# https://rstudio.github.io/shinydashboard/get_started.html
+
+install.packages("shinydashboard")
+
+# A dashboard has three parts: 
+# 1 a header, 
+# 2 a sidebar, 
+# 3 and a body. 
+# Here’s the most minimal possible UI for a dashboard page.
+
+## app.R ##
+library(shiny)
+library(shinydashboard)
+
+ui <- dashboardPage(
+  dashboardHeader(),
+  dashboardSidebar(),
+  dashboardBody()
+)
+
+# You can quickly view it at the R console by using the shinyApp() function. 
+# (You can also use this code as a single-file app).
+
+server <- function(input, output) { }
+shinyApp(ui, server)
+
---
+++ 08_shinydashboard/08_shinydashboard_app_01.R
@@ -0,0 +1,45 @@
+# shinydashboard
+# Para Curso visualización avanzada master UNED 2026
+
+# A partir de
+# https://rstudio.github.io/shinydashboard/get_started.html
+
+# install.packages("shinydashboard")
+
+# A dashboard has three parts: 
+# 1 a header, 
+# 2 a sidebar, 
+# 3 and a body. 
+# Here’s the most minimal possible UI for a dashboard page.
+
+## app.R ##
+library(shinydashboard)
+
+ui <- dashboardPage(
+  dashboardHeader(title = "Basic dashboard"),
+  dashboardSidebar(),
+  dashboardBody(
+    # Boxes need to be put in a row (or column)
+    fluidRow(
+      box(plotOutput("plot1", height = 250)),
+      
+      box(
+        title = "Controls",
+        sliderInput("slider", "Number of observations:", 1, 100, 50)
+      )
+    )
+  )
+)
+
+server <- function(input, output) {
+  set.seed(122)
+  histdata <- rnorm(500)
+  
+  output$plot1 <- renderPlot({
+    data <- histdata[seq_len(input$slider)]
+    hist(data)
+  })
+}
+
+shinyApp(ui, server)
+
